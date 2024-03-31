16d33ee2b4b9a0a25108b67292cd8ef33c087b6a
Brett <2445631968@qq.com>
2024-03-31 15:21:29 +0800

    Site updated: 2024-03-31 15:21:28

--- js/runtime.js
+++ js/runtime.js
@@ -1,4 +1,4 @@
-var now = new Date();
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>var now = new Date();
 function createtime() {
     var grt = new Date("03/29/2024 00:00:00");
     now.setTime(now.getTime() + 250);
